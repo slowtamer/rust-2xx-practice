9099d965d73e84931cb837d081e7c7da3ef11275
slowtamer <oleksandrbozhko5050@gmail.com>
2024-09-27 13:06:38 +0300

    chapter2.rs

--- src/chapter4.rs
+++ src/chapter4.rs
@@ -1,5 +1,14 @@
-/// https://practice.course.rs/basic-types/numbers.html
-#[test]
-fn test41() {
+// Don't modify code in main!
+fn main() {
+    let s1 = String::from("Hello world");
+    let s2 = take_ownership(s1);
 
+    println!("{}", s2); // Тепер s2 може бути використане
 }
+
+// Only modify the code below!
+fn take_ownership(s: String) -> String { // Додаємо повернення типу String
+    println!("{}", s);
+    s // Повертаємо s назад
+}
+
